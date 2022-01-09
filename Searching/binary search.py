202969d3463c3a313822da58b42eaa36dbee5b0a
Dhanush <96831879+dhanushbt@users.noreply.github.com>
2022-01-09 13:12:16 +0530

    Update binary search.py

--- Searching/binary search.py
+++ Searching/binary search.py
@@ -5,6 +5,6 @@ def binary_search(value,list):
   if value==list[mid]:
     return True
   if value>=list[mid]:
-    return binary_search(value,l[m+1:])
+    return binary_search(value,list[m+1:])
   else:
-    return binary_search(value,l[:m])
+    return binary_search(value,list[:m])
